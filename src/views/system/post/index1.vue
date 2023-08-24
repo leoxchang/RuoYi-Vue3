feaf7d804e31137c67b22019a0695cfb4fad1ae3
张新磊 <leo_zxl@163.com>
2023-08-24 17:11:22 +0800

    优化代码

--- src/views/system/post/index1.vue
+++ src/views/system/post/index1.vue
@@ -144,153 +144,134 @@
    </div>
 </template>
 
-<script>
-import { listPost, getPost, delPost, addPost, updatePost } from "@/api/system/post";
-export default {
-    name: "Post",
-    dicts: ['sys_normal_disable'],
-    data() {
-        return {
-            // 遮罩层
-            loading: true,
-            // 选中数组
-            ids: [],
-            // 非单个禁用
-            single: true,
-            // 非多个禁用
-            multiple: true,
-            // 显示搜索条件
-            showSearch: true,
-            // 总条数
-            total: 0,
-            // 岗位表格数据
-            postList: [],
-            // 弹出层标题
-            title: "",
-            // 是否显示弹出层
-            open: false,
-            // 查询参数
-            queryParams: {
-                pageNum: 1,
-                pageSize: 10,
-                postCode: undefined,
-                postName: undefined,
-                status: undefined
-            },
-            // 表单参数
-            form: {},
-            // 表单校验
-            rules: {
-                postName: [
-                    { required: true, message: "岗位名称不能为空", trigger: "blur" }
-                ],
-                postCode: [
-                    { required: true, message: "岗位编码不能为空", trigger: "blur" }
-                ],
-                postSort: [
-                    { required: true, message: "岗位顺序不能为空", trigger: "blur" }
-                ]
-            }
-        };
-    },
-    created() {
-        this.getList();
-    },
-    methods: {
-        /** 查询岗位列表 */
-        getList() {
-            this.loading = true;
-            listPost(this.queryParams).then(response => {
-                this.postList = response.data.rows;
-                this.total = response.data.total;
-                this.loading = false;
-            });
-        },
-        // 取消按钮
-        cancel() {
-            this.open = false;
-            this.reset();
-        },
-        // 表单重置
-        reset() {
-            this.form = {
-                postId: undefined,
-                postCode: undefined,
-                postName: undefined,
-                postSort: 0,
-                status: "0",
-                remark: undefined
-            };
-            this.resetForm("form");
-        },
-        /** 搜索按钮操作 */
-        handleQuery() {
-            this.queryParams.pageNum = 1;
-            this.getList();
-        },
-        /** 重置按钮操作 */
-        resetQuery() {
-            this.resetForm("queryForm");
-            this.handleQuery();
-        },
-        // 多选框选中数据
-        handleSelectionChange(selection) {
-            this.ids = selection.map(item => item.postId)
-            this.single = selection.length!=1
-            this.multiple = !selection.length
-        },
-        /** 新增按钮操作 */
-        handleAdd() {
-            this.reset();
-            this.open = true;
-            this.title = "添加岗位";
-        },
-        /** 修改按钮操作 */
-        handleUpdate(row) {
-            this.reset();
-            const postId = row.postId || this.ids
-            getPost(postId).then(response => {
-                this.form = response.data;
-                this.open = true;
-                this.title = "修改岗位";
-            });
-        },
-        /** 提交按钮 */
-        submitForm: function() {
-            this.$refs["form"].validate(valid => {
-                if (valid) {
-                    if (this.form.postId != undefined) {
-                        updatePost(this.form).then(response => {
-                            this.$modal.msgSuccess("修改成功");
-                            this.open = false;
-                            this.getList();
-                        });
-                    } else {
-                        addPost(this.form).then(response => {
-                            this.$modal.msgSuccess("新增成功");
-                            this.open = false;
-                            this.getList();
-                        });
-                    }
-                }
-            });
-        },
-        /** 删除按钮操作 */
-        handleDelete(row) {
-            const postIds = row.postId || this.ids;
-            this.$modal.confirm('是否确认删除岗位编号为"' + postIds + '"的数据项？').then(function() {
-                return delPost(postIds);
-            }).then(() => {
-                this.getList();
-                this.$modal.msgSuccess("删除成功");
-            }).catch(() => {});
-        },
-        /** 导出按钮操作 */
-        handleExport() {
-            this.download('system/post/export', {
-                ...this.queryParams
-            }, `post_${new Date().getTime()}.xlsx`)
-        }
+<script setup name="Post">
+import { listPost, addPost, delPost, getPost, updatePost } from "@/api/system/post";
+
+const { proxy } = getCurrentInstance();
+const { sys_normal_disable } = proxy.useDict("sys_normal_disable");
+
+const postList = ref([]);
+const open = ref(false);
+const loading = ref(true);
+const showSearch = ref(true);
+const ids = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    postCode: undefined,
+    postName: undefined,
+    status: undefined
+  },
+  rules: {
+    postName: [{ required: true, message: "岗位名称不能为空", trigger: "blur" }],
+    postCode: [{ required: true, message: "岗位编码不能为空", trigger: "blur" }],
+    postSort: [{ required: true, message: "岗位顺序不能为空", trigger: "blur" }],
+  }
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询岗位列表 */
+function getList() {
+  loading.value = true;
+  listPost(queryParams.value).then(response => {
+    postList.value = response.data.rows;
+    total.value = response.data.total;
+    loading.value = false;
+  });
+}
+/** 取消按钮 */
+function cancel() {
+  open.value = false;
+  reset();
+}
+/** 表单重置 */
+function reset() {
+  form.value = {
+    postId: undefined,
+    postCode: undefined,
+    postName: undefined,
+    postSort: 0,
+    status: "0",
+    remark: undefined
+  };
+  proxy.resetForm("postRef");
+}
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+/** 重置按钮操作 */
+function resetQuery() {
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+/** 多选框选中数据 */
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.postId);
+  single.value = selection.length != 1;
+  multiple.value = !selection.length;
+}
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加岗位";
+}
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const postId = row.postId || ids.value;
+  getPost(postId).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改岗位";
+  });
+}
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["postRef"].validate(valid => {
+    if (valid) {
+      if (form.value.postId != undefined) {
+        updatePost(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        });
+      } else {
+        addPost(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        });
+      }
     }
-};
+  });
+}
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const postIds = row.postId || ids.value;
+  proxy.$modal.confirm('是否确认删除岗位编号为"' + postIds + '"的数据项？').then(function() {
+    return delPost(postIds);
+  }).then(() => {
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch(() => {});
+}
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download("system/post/export", {
+    ...queryParams.value
+  }, `post_${new Date().getTime()}.xlsx`);
+}
+
+getList();
 </script>
