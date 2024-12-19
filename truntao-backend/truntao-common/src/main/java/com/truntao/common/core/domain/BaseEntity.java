31850e20e3c014a118bc344cd263e0ad9e268f1e
zhangxinlei <leo_zxl@163.com>
2024-12-19 11:38:55 +0800

    fix: add timezone

--- truntao-backend/truntao-common/src/main/java/com/truntao/common/core/domain/BaseEntity.java
+++ truntao-backend/truntao-common/src/main/java/com/truntao/common/core/domain/BaseEntity.java
@@ -38,7 +38,7 @@ public class BaseEntity implements Serializable {
     /**
      * 创建时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @TableField("create_time")
     private Date createTime;
 
@@ -51,7 +51,7 @@ public class BaseEntity implements Serializable {
     /**
      * 更新时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
     /**
