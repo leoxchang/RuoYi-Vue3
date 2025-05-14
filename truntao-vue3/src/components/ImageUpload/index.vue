ffa4d26c5e91fc6bbf94b49b7bade3d679db9bcf
zhangxinlei <leo_zxl@163.com>
2025-05-14 16:25:39 +0800

    fix: 修复上传组件被多次引用拖动仅对第一个有效的问题

--- truntao-vue3/src/components/ImageUpload/index.vue
+++ truntao-vue3/src/components/ImageUpload/index.vue
@@ -235,7 +235,7 @@ function listToString(list, separator) {
 onMounted(() => {
   if (props.drag && !props.disabled) {
     nextTick(() => {
-      const element = document.querySelector('.el-upload-list')
+      const element = proxy.$refs.imageUpload?.$el?.querySelector('.el-upload-list')
       Sortable.create(element, {
         onEnd: (evt) => {
           const movedItem = fileList.value.splice(evt.oldIndex, 1)[0]
