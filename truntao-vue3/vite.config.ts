5c03d2a8304258c17f1c5c715fb5e9c060dcb11e
张新磊 <leo_zxl@163.com>
2025-06-07 09:57:51 +0800

    feat: vite.config.js转换为ts格式

--- truntao-vue3/vite.config.ts
+++ truntao-vue3/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv, UserConfig, ConfigEnv } from 'vite'
 import path from 'path'
 import createVitePlugins from './vite/plugins'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode, command }) => {
+export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
   const env = loadEnv(mode, process.cwd())
   const { VITE_APP_ENV } = env
   return {
@@ -45,14 +45,14 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: 'http://192.168.88.8:6060',
+          target: 'http://localhost:8080',
           changeOrigin: true,
-          rewrite: path => path.replace(/^\/dev-api/, 'system/')
+          rewrite: (path: string) => path.replace(/^\/dev-api/, '')
         },
         '/v3/api-docs': {
           target: 'http://localhost:8080',
           changeOrigin: true,
-          rewrite: path => path
+          rewrite: (path: string) => path
         }
       }
     },
@@ -63,7 +63,7 @@ export default defineConfig(({ mode, command }) => {
           {
             postcssPlugin: 'internal:charset-removal',
             AtRule: {
-              charset: (atRule) => {
+              charset: (atRule: any) => {
                 if (atRule.name === 'charset') {
                   atRule.remove();
                 }
